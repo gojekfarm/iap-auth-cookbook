1c21e154accded8e1fcf93ea174d815754d158a9
Tasdik Rahman <tasdik.rahman@go-jek.com>
2019-02-18 18:39:55 +0530

    fix(version): add mission prefix

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@
 default['iap-auth']['version'] = ENV['IAP_AUTH_VERSION'] || '0.1.0'
 
 default['iap-auth']['release_gz_file'] = "iap_auth_#{node['iap-auth']['version']}_Linux_x86_64.tar.gz"
-default['iap-auth']['github_release_url'] = "https://github.com/gojekfarm/iap_auth/releases/download/#{node['iap-auth']['version']}/#{node['iap-auth']['release_gz_file']}"
+default['iap-auth']['github_release_url'] = "https://github.com/gojekfarm/iap_auth/releases/download/v#{node['iap-auth']['version']}/#{node['iap-auth']['release_gz_file']}"
 default['iap-auth']['iap_auth_dir'] = '/opt/iap_auth'
 default['iap-auth']['service_account_path'] = "#{node['iap-auth']['iap_auth_dir']}/service_account_credentials.json"
 
