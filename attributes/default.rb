2a228b5608f17b39b3e522d153f6194433762116
Tasdik Rahman <tasdik.rahman@go-jek.com>
2019-02-18 18:27:44 +0530

    fix(attributes); version

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2019, The Authors, All Rights Reserved.
 #
 
-default['iap-auth']['version'] = ENV['IAP_AUTH_VERSION'] || 'v0.1.0'
+default['iap-auth']['version'] = ENV['IAP_AUTH_VERSION'] || '0.1.0'
 
 default['iap-auth']['release_gz_file'] = "iap_auth_#{node['iap-auth']['version']}_Linux_x86_64.tar.gz"
 default['iap-auth']['github_release_url'] = "https://github.com/gojekfarm/iap_auth/releases/download/#{node['iap-auth']['version']}/#{node['iap-auth']['release_gz_file']}"
@@ -17,4 +17,3 @@ default['iap-auth']['service_account_credentials'] = ENV['SERVICE_ACCOUNT_CREDEN
 default['iap-auth']['client_id'] = ENV['CLIENT_ID']
 default['iap-auth']['port'] = ENV['PORT'] || '8080'
 default['iap-auth']['refresh_time_seconds'] = ENV['REFRESH_TIME_SECONDS'] || '3600'
-
